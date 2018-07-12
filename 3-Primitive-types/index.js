17c08538232259bdb0ade9364d60732e963d090e
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-13 00:26:51 +1200

    Changes to js on 2

--- 3-Primitive-types/index.js
+++ 3-Primitive-types/index.js
@@ -1,4 +1,5 @@
-// this is my first js code 
-const intrestRate = 0.3;
-
-console.log(intrestRate);
+let  name = "Mosh" ;  // String Literal
+let age = 30 ; //Number Literal
+let isAproved = true ;  // boolean literal
+let firstName  = undefined ;
+let selectColour  = null ;    
