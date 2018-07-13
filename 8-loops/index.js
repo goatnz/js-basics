0665761e2b56261753f702a5b344fa697631026a
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-13 23:17:10 +1200

    loop 1

--- 8-loops/index.js
+++ 8-loops/index.js
@@ -1,10 +1,25 @@
-let a = 'red';
-let b = 'blue';
+// // // for loop 
 
-let c = a ; 
-a = b ; 
-b = c ;
+// // for (let i = 0;  i <= 5; i++) {
+// //      if (i % 2 !== 0 ) console.log(i) ;
+    
+// // }
+
+// // // while 
+
+// // let i = o ; 
+// // while (i <= 5){
+// //     if (i % 2 !== 0 ) console.log(i) ;
+// //     i++  
+// // }
 
 
-console.log(a) ; 
-console.log(b) ; 
+// // // do-while
+//  NB : allways evaluate once 
+
+let i = 9  ; 
+do {
+    if (i % 2 !== 0 ) console.log(i)
+    i++ ; 
+    
+} while (i <= 5);
