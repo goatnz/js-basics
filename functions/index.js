f88cfa6cb8052d25e06c4b599b5b94e245384c90
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-13 16:41:04 +1200

    last function file

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
+let nb = 4 ;
 
+// preforme a task 
 function greet(name,lastName)
 {
     console.log('Hello '+ name + '' + lastName)
 }
 
+// calculate a value 
+
+function square(number) {
+
+    return number * number ; 
+    
+}
+
+
+numbers = square(nb)
+console.log(numbers)
+
 greet('Hone','Komarkowski') ;
-greet('Hone Komarkowski') ;
+
+
 
