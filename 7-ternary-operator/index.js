2982557fb8673095934358fca3b00fb567d564ca
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-13 23:06:09 +1200

    loops

--- 7-ternary-operator/index.js
+++ 7-ternary-operator/index.js
@@ -1,5 +1,9 @@
-let x = 10 ;
-let y = 3 ;
+// if a customer has more than 100 points 
+// they are gold customers otherwise 
+// they are silver 
 
-console.log(x+y)
+let points = 99 ; 
+let type = points > 100 ? 'gold' : 'silver' ; 
+
+console.log(type) ; 
 
