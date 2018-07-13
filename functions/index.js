bcf8d4381ae4acc7eb4391409c87bffb5cd53754
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-13 16:24:28 +1200

    update

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+let  name = "Mosh" ;  // String Literal
+let age = 30 ; //Number Literal
+let isAproved = true ;  // boolean literal
+let firstName  = undefined ;
+let selectColour  = null ;    
--- index.js
+++
@@ -1,4 +0,0 @@
-let     selectedColours = ['red','blue'];
-selectedColours [2] = 1 ;
-console.log(selectedColours.length)
-
