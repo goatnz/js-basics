56f5ca9586cd150f0390c79391e718c84994d129
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-13 16:33:56 +1200

    functions

--- Arays/index.js
+++ Arays/index.js
@@ -1,5 +1,4 @@
-let  name = "Mosh" ;  // String Literal
-let age = 30 ; //Number Literal
-let isAproved = true ;  // boolean literal
-let firstName  = undefined ;
-let selectColour  = null ;    
+let     selectedColours = ['red','blue'];
+selectedColours [2] = 1 ;
+console.log(selectedColours.length)
+
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
-let  name = "Mosh" ;  // String Literal
-let age = 30 ; //Number Literal
-let isAproved = true ;  // boolean literal
-let firstName  = undefined ;
-let selectColour  = null ;    
+
+function greet(name,lastName)
+{
+    console.log('Hello '+ name + '' + lastName)
+}
+
+greet('Hone','Komarkowski') ;
+greet('Hone Komarkowski') ;
+
