8e4f34b1e9a8bbc118fa797d4aab9cf1911c1c25
goatnz <30343617+goatnz@users.noreply.github.com>
2018-07-14 00:45:22 +1200

    demerit ex

---
+++ 104 exercise/index.js
@@ -0,0 +1,25 @@
+// Speed Limit = 70
+// 5 -> 1 
+// math.floor(1.3)
+// 12 points -> suspended 
+
+
+checkSpeed(125);
+
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed <= speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
+    }
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+        console.log('suspended');
+    else
+        console.log('Points', points);
+
+
+}
